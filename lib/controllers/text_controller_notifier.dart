eebd7504535fcd5ca904915041e796be384d67e9
Daniel Choi <63138113+Daniel-Choi3810@users.noreply.github.com>
2023-01-30 11:10:21 -0500

    Changed AI model to ADA

--- lib/controllers/text_controller_notifier.dart
+++ lib/controllers/text_controller_notifier.dart
@@ -34,7 +34,7 @@ class TextControllerNotifier extends StateNotifier<String?> {
       logprobs: null,
       contentType: 'application/json',
       authorization: 'Bearer $_apiToken',
-      model: 'text-davinci-002',
+      model: 'text-ada-001',
       url: url,
       apiToken: _apiToken!,
       // stop: '. ',
