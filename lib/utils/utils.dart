1ed425be96c620976fd52ee1fd399c5deae3bb26
Daniel Choi <63138113+Daniel-Choi3810@users.noreply.github.com>
2023-02-09 16:23:11 -0500

    Changed white background shade and changed main home

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const dominant = Color(0xfff3f1f1);
+  static const dominant = Color(0xfff8f6f6);
   static const complementary = Color(0xff185A76);
   static const complementaryLight = Color(0xff6abde0);
   static const complementaryDark = Color(0xff13485e);
